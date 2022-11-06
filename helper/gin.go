8b88b855be5098636010ecf78aa6c7523f5d099f
Dominikus09 <andikadk02@gmail.com>
2022-11-06 13:44:22 +0700

    add new func

--- helper/gin.go
+++ helper/gin.go
@@ -19,10 +19,20 @@ func action(c *gin.Context) {
 	combin := name + " sedang " + aksi
 	c.JSON(http.StatusOK, combin)
 }
+func data(c *gin.Context) {
+	array := map[string]string{
+		"nama":  "Dominikus Andika Kurniawan",
+		"age":   "20",
+		"NIM":   "2440056972",
+		"Major": "Sistem Informasi dan Manajemen",
+	}
+	c.JSON(http.StatusOK, array)
+}
 
 func Api() {
 	r := gin.Default()
 	r.GET("/massage", massage)
 	r.GET("/kegiatan/:nama/:aksi", action)
+	r.GET("/data", data)
 	r.Run()
 }
